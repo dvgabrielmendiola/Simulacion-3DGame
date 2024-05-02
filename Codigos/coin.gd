55b9b3b3e694d83307a4abf0dc66a2197e014377
Gabriel Mendiola <gabriel.m.f.4@gmail.com>
2024-05-02 16:50:18 -0600

    F1

---
+++ Codigos/coin.gd
@@ -0,0 +1,17 @@
+extends Area3D
+const rotateSpeed = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_y(deg_to_rad(rotateSpeed))
+	#if has_overlapping_bodies():
+		#queue_free()
+
+
+func _on_body_entered(body):
+	queue_free() # Replace with function body.
